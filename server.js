cf805c614d851c721862576d0c4a922d7de15233
Dredog0239 <almays29@gmail.com>
2023-03-23 22:25:46 -0700

    updating my code.

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-
+const apiRoutes = require("./Routes/routeApi");
+const htmlRoutes = require("./Routes/htmlRoute");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-require("./RouteFiles/API-route")(app);
-require("./RouteFiles/Html-routes")(app);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+//require("./RouteFiles/API-route")(app);
+//require("./RouteFiles/Html-routes")(app);
 
 app.listen(PORT, function () {
   console.log("Application listening http://localhost:" + PORT);
