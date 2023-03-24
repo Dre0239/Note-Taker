ade44a60cf90dccd794b9a273395075630d1ba2b
Dredog0239 <almays29@gmail.com>
2023-03-23 21:28:48 -0700

    updating code.

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const allNotes = require("./db/db.json");
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
